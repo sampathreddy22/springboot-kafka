a07d923dbce09cf5fe03a3f34f3c434ad3ec9bd2
sampathreddy22 <sampathreddy.jonnala@gmail.com>
2020-06-23 16:42:01 -0400

    Addded Spring and maven framework

---
+++ src/main/java/com/aadev/springbootkafka/springbootKafka.java
@@ -0,0 +1,13 @@
+package com.aadev.springbootkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class springbootKafka {
+
+	public static void main(String[] args) {
+		SpringApplication.run(springbootKafka.class, args);
+	}
+
+}
---
+++ src/test/java/com/aadev/springbootkafka/springbootKafkaTests.java
@@ -0,0 +1,13 @@
+package com.aadev.springbootkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class springbootKafkaTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
